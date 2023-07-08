8174519b63ad8dd79c718aa3aacbd9e5c606044b
harrison <harrison.destefano@gmail.com>
2023-07-08 08:52:27 -0400

    chore: adele session mux clean up

--- middleware.go
+++ middleware.go
@@ -9,7 +9,6 @@ import (
 
 // Load and save session on each request
 func (a *Adel) SessionLoad(next http.Handler) http.Handler {
-	a.InfoLog.Println("SessionLoad called")
 	return a.Session.LoadAndSave(next)
 }
 
@@ -18,12 +17,6 @@ func (a *Adel) NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 	secure, _ := strconv.ParseBool(a.config.cookie.secure)
 
-	// TO DO
-	//
-	// How will this fit into API request since they do not need csrf?
-	//
-	// Be nice to do this on the route level and not the handler.
-	//
 	// Pattern of string that does not get a token
 	// https://github.com/justinas/nosurf/blob/master/exempt.go#L55
 	// ExemptPath(), ExemptGlob(), ExemptGlobs() ...
